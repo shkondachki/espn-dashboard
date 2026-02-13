e07c8ca9a9723e072e6c2bccb8d1e53101bab7cd
Tsvetomir Shkondachki <cvetomir.shk@gmail.com>
2026-02-13 13:07:41 +0200

    Moved RefineLayout outside router

---
+++ src/components/RefineLayout.tsx
@@ -0,0 +1,51 @@
+import { Outlet } from 'react-router-dom';
+import { Refine } from '@refinedev/core';
+import { ThemedLayoutV2, ThemedTitleV2, useNotificationProvider } from '@refinedev/mui';
+import routerBindings from '@refinedev/react-router-v6';
+import { dataProvider } from '@/api/refineDataProvider';
+import { AppHeader } from '@/components/AppHeader';
+import DashboardIcon from "@mui/icons-material/Dashboard";
+import GroupsIcon from "@mui/icons-material/Groups";
+import SportsSoccerIcon from '@mui/icons-material/SportsSoccer';
+
+export function RefineLayout() {
+  return (
+    <Refine
+      dataProvider={dataProvider}
+      routerProvider={routerBindings}
+      notificationProvider={useNotificationProvider}
+      resources={[
+        {
+          name: 'dashboard',
+          list: '/',
+          meta: {
+            label: 'Scoreboard',
+            icon: <DashboardIcon />,
+          },
+        },
+        {
+          name: 'teams',
+          list: '/teams',
+          meta: {
+            label: 'Teams',
+            icon: <GroupsIcon />,
+          },
+        },
+      ]}
+      options={{ syncWithLocation: true }}
+    >
+      <ThemedLayoutV2
+        Header={AppHeader}
+        Title={({ collapsed }) => (
+          <ThemedTitleV2
+            text="ESPN"
+            icon={<SportsSoccerIcon />}
+            collapsed={collapsed}
+          />
+        )}
+      >
+        <Outlet />
+      </ThemedLayoutV2>
+    </Refine>
+  );
+}
